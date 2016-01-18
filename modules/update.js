254e0f82a6d38ce6017665abbf1e9b61c133f7a9
nacgarg <nachiketa.gargi@gmail.com>
2016-01-18 11:37:47 -0800

    added desc and fixed typo

--- modules/update.js
+++ modules/update.js
@@ -5,8 +5,9 @@ let exec = require('child_process').exec
 
 module.exports = {
 	command: 'update',
-	permissionRequired: p.LEVEL_ADMIN
+	permissionRequired: p.LEVEL_ADMIN,
 	handler(bot, args, msg) {
 		exec('git pull');
-	}
+	},
+	desc: "Update the bot."
 }
