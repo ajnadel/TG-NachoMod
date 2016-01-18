3d7a797e4b40b804e7e7ce3cf3970e10f36eeab9
nacgarg <nachiketa.gargi@gmail.com>
2016-01-18 09:50:46 -0800

    started news, not sure if it works yet

---
+++ modules/news.js
@@ -0,0 +1,35 @@
+'use strict';
+
+let Watcher = require('rss-watcher');
+let RSS_LINK = 'http://www.npr.org/rss/rss.php?id=1001'
+let newsEnabled = false;
+let w = new Watcher(RSS_LINK);
+
+let chatIDs = []
+
+w.on('new article', (a) => {
+	if (newsEnabled) {
+		chatIDs.forEach((id) => {
+			bot.sendMessage(id, "New article: " + a.toString())
+		}) //send a message to each chat id
+	}
+})
+
+w.on("error", (error) => {
+	console.log(error)
+})
+
+module.exports = {
+	command: 'news',
+	handler(bot, args, msg) {
+		if (!newsEnabled) {
+			newsEnabled = true;
+			chatIDs.push(msg.chat.id)
+			bot.sendMessage(msg.chat.id, "Enabled news")
+		} else {
+			newsEnabled = false;
+			bot.sendMessage(msg.chat.id, "Disabled news")
+			chatIDs.splice(chatIDs.indexOf(msg.chat.id), 1)
+		}
+	}
+}
