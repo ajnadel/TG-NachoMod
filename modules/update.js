8cb36a7934e32209e8f1103ab6e121aa24daf2df
nacgarg <nachiketa.gargi@gmail.com>
2016-01-18 11:36:18 -0800

    update

---
+++ modules/update.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let p = require('../permissions')
+let exec = require('child_process').exec
+
+module.exports = {
+	command: 'update',
+	permissionRequired: p.LEVEL_ADMIN
+	handler(bot, args, msg) {
+		exec('git pull');
+	}
+}
