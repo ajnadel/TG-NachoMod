1defe8166b37a0080a6e0d89bcaf1c1e66cab131
nacgarg <nachiketa.gargi@gmail.com>
2016-01-17 21:32:11 -0800

    updated whoami

--- modules/whoami.js
+++ modules/whoami.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	command: 'whoami',
-	handler: (bot, args, msg) => {
+	handler(bot, args, msg) {
 		console.log(msg)
 		bot.sendMessage(msg.chat.id, `*First Name:* ${msg.from.first_name}\n*Last Name:* ${msg.from.last_name || "n/a"}\n*Username* ${msg.from.username || "No username specified."}\n*ID:* ${msg.from.id}`, {parse_mode: "Markdown"})
 	}
